0c3f9b2308da0002404c7a2e7ca15aeb21d649de
Your Name <you@example.com>
2019-10-16 11:59:37 +0200

    Anton är dum

---
+++ app/src/main/java/anton/vastkustit/cloud_remover/Main3Activity.java
@@ -0,0 +1,18 @@
+package anton.vastkustit.cloud_remover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        TextView textView = (TextView)findViewById(R.id.hahaha);
+        textView.setText("tryckningar");
+    }
+}
