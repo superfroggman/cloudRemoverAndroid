760fcd665ee79ce85d07319638d2692f34d5ae58
Your Name <you@example.com>
2019-10-22 11:48:44 +0200

    reomve copy paste

--- app/src/main/java/anton/vastkustit/cloud_remover/Main3Activity.java
+++ app/src/main/java/anton/vastkustit/cloud_remover/Main3Activity.java
@@ -15,21 +15,14 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        /*
         TextView textView = (TextView)findViewById(R.id.hahaha);
         textView.setText("tryckningar");
 
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
-         */
 
-        String shareBody = "Here is the share content body";
-        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sharingIntent.setType("text/plain");
-        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-        startActivity(Intent.createChooser(sharingIntent, getResources().getString(R.string.share_using)));
+        //cooll
 
     }
 
